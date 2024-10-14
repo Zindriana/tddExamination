92d2ccfef5b99903f66b32bab2d2870c470d0023
Adam <Adam.sturesson@live.se>
2024-10-14 14:31:50 +0200

    Player variables

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,6 +1,18 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
 
+    private String name;
+    private int currentRoom;
+    private List<Item> backpack = new ArrayList<Item>();
+
+    public Player(String name){
+        this.name = name;
+    }
 
 }
+
+
