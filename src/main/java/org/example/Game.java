d6d80df26c25d77488a416668787598441132f46
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-10-14 14:35:22 +0200

    added variables, constructor and getitem to Game class

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,4 +1,24 @@
 package org.example;
 
+import java.util.List;
+
 public class Game {
+
+    private final Player player;
+    private final Item[] items = new Item[10]; //change this value later when we now how many different items exist in the game
+    private final Building building = new Building();
+
+    public Game(String name) {
+        this.player = new Player(name);
+    }
+
+    public Item getItem(String itemName) {
+        for(Item item : items){
+            if(item.equals(itemName)){
+                return item;
+            }
+        }
+        return null;
+    }
+
 }
