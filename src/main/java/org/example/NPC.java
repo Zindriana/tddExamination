cd77b8dfa63c7cfff53d7888f3af0ce971ecbc05
unknown <g.tanzharikova@gmail.com>
2024-10-15 00:03:20 +0200

    Add NPC implementation and tests

--- src/main/java/org/example/NPC.java
+++ src/main/java/org/example/NPC.java
@@ -1,4 +1,24 @@
 package org.example;
 
-public class NPC {
+public class NPC extends Player {
+
+    private String description;
+
+    public NPC(String name) {
+        super("NPC_" + name);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public void checkBackpack() {
+        System.out.println("checkBackpack method is not supported for NPC");
+    }
 }
+
--- src/test/java/org/example/NPCTest.java
+++ src/test/java/org/example/NPCTest.java
@@ -1,7 +1,55 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class NPCTest {
+    private NPC npc;
 
+    @BeforeEach
+    void setUp() {
+        npc = new NPC("Tom");
+        npc.setCurrentRoom(1);
+        npc.setDescription("npc description");
+        System.out.println(npc.getName());
+    }
+
+
+    @Test
+    public void test_getName() {
+        assertEquals("NPC_Tom", npc.getName());
+    }
+
+    @Test
+    public void test_getName_withError() {
+        assertNotEquals("NPC Tom", npc.getName());
+    }
+
+    @Test
+    public void test_moveForward() {
+        npc.moveForward();
+        assertEquals(2, npc.getCurrentRoom());
+    }
+
+    @Test
+    public void test_moveForwardx3() {
+        npc.moveForward();
+        npc.moveForward();
+        npc.moveForward();
+        assertEquals(4, npc.getCurrentRoom());
+    }
+
+    @Test
+    public void test_moveBackward() {
+        npc.moveBack();
+        assertEquals(0, npc.getCurrentRoom());
+    }
+
+    @Test
+    public void test_getDescription() {
+        assertEquals("npc description", npc.getDescription());
+    }
 }
