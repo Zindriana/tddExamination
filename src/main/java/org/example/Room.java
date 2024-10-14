9f8cc8c2d02e819e450c19d7c07f16196db79b8d
Yoe <dembouz719@gmail.com>
2024-10-14 14:26:02 +0200

    Created fields, constructors and getters for Room class

--- src/main/java/org/example/Room.java
+++ src/main/java/org/example/Room.java
@@ -1,4 +1,33 @@
 package org.example;
 
+import java.util.List;
+
 public class Room {
+
+    //fields
+    private int roomNumber;
+    private String name;
+    private String description;
+    private List<Item> items;
+
+    //constructors
+    public Room(int roomNumber, String name, String description)    {
+        this.roomNumber = roomNumber;
+        this.name = name;
+        this.description = description;
+    }
+
+    //getters
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
