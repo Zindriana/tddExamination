ed5b012ed7b12716dab98484cc14b8dd6606913c
Yoe <dembouz719@gmail.com>
2024-10-15 10:06:48 +0200

    Created test for get Room Number

--- src/test/java/org/example/RoomTest.java
+++ src/test/java/org/example/RoomTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class RoomTest {
 
@@ -18,9 +19,37 @@ class RoomTest {
         assertEquals(1, expectedRoomNumber);
     }
 
+    @Test
+    void testGetRoomName() {
+        //given
+        Room room = new Room(2,"Test Room", "This is a test room");
+
+        //when
+        String expectedRoomName = room.getName();
+
+        //then
+        assertEquals("Test Room", expectedRoomName);
+    }
+
+    @Test
+    void testGetRoomDescription() {
+        //given
+        Room room = new Room(1, "Test Room", "This is a test room");
+        //when
+        String expectedRoomDescription = room.getDescription();
+
+        //then
+        assertEquals("This is a test room", expectedRoomDescription);
+    }
+
+
+    @Test
+    void test() {
+        //given mocked room
 
 
 
+    }
 
 
 }
