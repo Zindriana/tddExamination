c1f5423712979a59805440af4f83d2d7152d7f10
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-10-15 09:37:24 +0200

    made a change in the GameTest class

--- src/test/java/org/example/GameTest.java
+++ src/test/java/org/example/GameTest.java
@@ -32,9 +32,10 @@ class GameTest {
             System.setOut(new PrintStream(outContent));
             game.startGame();
 
+
             assertEquals(inputName, game.getPlayer().getName());
             assertEquals(
-                    "Choose your name\r\nHello " + game.getPlayer().getName() +
+                    "Choose your name\nHello " + game.getPlayer().getName() +
                             ". Welcome to my dungeon. To get out you need to solve my puzzles!",
                     outContent.toString().trim());
         } finally {
