721f3f086ed2046cfb631a2f539db61e6060b7b5
MahmoudAbed2 <148431502+MahmoudAbed2@users.noreply.github.com>
2024-10-16 19:35:56 +0200

    Update Item.java
    ändrade Item

--- src/main/java/org/example/Item.java
+++ src/main/java/org/example/Item.java
@@ -1,50 +1,40 @@
 package org.example;
 
 public class Item {
- 
-    // Attribut för namn och beskrivning av föremålet
     private String name;
     private String description;
 
-    // Konstruktor för att skapa ett Item-objekt
+    // Konstruktor för att skapa ett nytt Item med namn och beskrivning
     public Item(String name, String description) {
         this.name = name;
         this.description = description;
     }
 
-    // Metod för att plocka upp föremålet
-    public void pickUp() {
-        System.out.println("You have picked up: " + name);
-        // Lägg till logik för att lägga föremålet i spelarens inventarie
-    }
-
-    // Metod för att använda föremålet
-    public void use() {
-        System.out.println("You have used the " + name);
-        // Lägg till logik för vad som händer när föremålet används
-    }
-
-    // Metod för att undersöka föremålet
-    public void investigateItem() {
-        System.out.println("You investigate the " + name + ". " + description);
-    }
-
-    // Getters för name och description
-
+    // Getter för namnet
     public String getName() {
         return name;
     }
 
+    // Getter för beskrivningen
     public String getDescription() {
         return description;
     }
 
-    // Setters för name och description
+    // Setter för att ändra namnet (om det behövs)
     public void setName(String name) {
         this.name = name;
     }
 
+    // Setter för att ändra beskrivningen (om det behövs)
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
     }
 }
