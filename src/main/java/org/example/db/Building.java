8420be34261b0a28b3bc292b47f4022b4ee07b4c
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-10-17 17:16:29 +0200

    added two items and a npc to the starting room

--- src/main/java/org/example/db/Building.java
+++ src/main/java/org/example/db/Building.java
@@ -5,13 +5,20 @@ import org.example.models.Room;
 public class Building {
 
     private final Room[] rooms = new Room[4];
+    private final NpcDB npcDB = new NpcDB();
+    private final ItemDB itemDB = new ItemDB();
 
     public Building(){
-
+        //adding rooms
         rooms[0] = new Room(0, "Starting room");
         rooms[1] = new Room(1, "Middle room");
         rooms[2] = new Room(2, "Third room");
         rooms[3] = new Room(3, "Last room");
+
+        //adding stuff to the rooms
+        rooms[0].setNpcInRoom(npcDB.getNPC("Hooded figure"));
+        rooms[0].itemsInRoom.add(itemDB.getItem("Die"));
+        rooms[0].itemsInRoom.add(itemDB.getItem("Candle"));
     }
 
     public int getNumberOfRooms() {
