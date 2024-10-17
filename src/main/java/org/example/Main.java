bc1650e2eac74f69a8fb620935a2caf0c8f23f23
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-10-18 01:46:54 +0200

    added a TODO in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,11 @@ package org.example;
 import org.example.game.Game;
 import org.example.game.GameUI;
 
+/*TODO create a folder for npc and create separate npc that extends the NPC-class
+*  Move the current conversation from the NPC class into a specific npc extended from the NPC-class
+*  - Delete the y/n question when checking backpack and instead directly go to choose an item to use,
+*       add an option to not use an item in the use item-menu after checking backpack.
+*   - Add code to get a list of all RoomObjects in a room when investigating, almost copy paste of the Item-functionality*/
 public class Main {
 
     public static void main(String[] args) {
