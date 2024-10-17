e8655d43967f5af27bc9707e181af5b2a15a46fd
Adam <Adam.sturesson@live.se>
2024-10-17 14:55:15 +0200

    changed the for loop to for i loop in getListOfItems to print out the index numbers for the items in the list

--- src/main/java/org/example/Room.java
+++ src/main/java/org/example/Room.java
@@ -57,8 +57,8 @@ public class Room {
         if (itemsInRoom.isEmpty()) {
             System.out.println("There are no items in this room.");
         } else {
-            for (Item item : itemsInRoom) {
-                System.out.println(item.getName());
+            for (int i = 0; i < itemsInRoom.size(); i++) {
+                System.out.println((i) + ". " + itemsInRoom.get(i).getName());
             }
         }
     }
