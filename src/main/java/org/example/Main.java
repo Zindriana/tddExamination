bad4bfc87328da9a19b633cbf178894719e2558f
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-10-18 15:36:49 +0200

    updated the todo list

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import org.example.game.Clue;
 import org.example.game.Game;
 import org.example.game.GameUI;
 
@@ -8,7 +7,9 @@ import org.example.game.GameUI;
 *  Move the current conversation from the NPC class into a specific npc extended from the NPC-class
 *  - Delete the y/n question when checking backpack and instead directly go to choose an item to use,
 *       add an option to not use an item in the use item-menu after checking backpack.
-*   - Add code to get a list of all RoomObjects in a room when investigating, almost copy paste of the Item-functionality*/
+*   - Add code to get a list of all RoomObjects in a room when investigating, almost copy paste of the Item-functionality
+*   - Create room three content, ice room
+*   - finish the interact with room object functionality (remember to use setDescription when stuff change*/
 public class Main {
 
     public static void main(String[] args) {
