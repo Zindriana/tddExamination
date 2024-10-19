c7cb219859270bb1b392255d4cb154428529ff91
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-10-19 02:22:02 +0200

    created the SmallShiningThing class

---
+++ src/main/java/org/example/models/RoomObjects/SmallShiningThing.java
@@ -0,0 +1,21 @@
+package org.example.models.RoomObjects;
+
+import org.example.models.Player;
+import org.example.models.Room;
+
+public class SmallShiningThing extends RoomObject {
+
+    public SmallShiningThing() {
+        super("Small Shining Thing");
+    }
+
+    @Override
+    public void interactWithObject(Room room, Player player) {
+        System.out.println("You see a small shining thing among some stones.\n" +
+                "It is a white bright six sided die that emit a weak light. \n" +
+                "You hear the hooded figure gives away a silent chuckle when \n" +
+                "you put it in your pocket.");
+        Player.backpack.add(room.getItem("Die"));
+        room.objectsInRoom.remove(room.getRoomObject("SmallShiningThing"));
+        }
+    }
