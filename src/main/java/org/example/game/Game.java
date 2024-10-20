f189069f3c9c261962d4e9b7d257f4c9c2168243
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-10-20 14:42:42 +0200

    removed pick up an item from the investigateRoom() method

--- src/main/java/org/example/game/Game.java
+++ src/main/java/org/example/game/Game.java
@@ -35,24 +35,24 @@ public class Game {
         getRoom(player.getCurrentRoom()).investigateRoom();
 
         System.out.println("\nWhat do you want to do?");
-        System.out.println("1.Pick up an item\n2.Interact with an object\n3.Interact with the npc\n4.Nothing");
+        System.out.println("1.Interact with an object\n2.Interact with the npc\n3.Nothing");
         Scanner sc = new Scanner(System.in);
         String choice = sc.nextLine().trim();
         switch (choice) {
-            case "1": //Pick up an item
-                System.out.println("Enter the index of the item: ");
-                int itemIndex = sc.nextInt();
-                try {
-                    getRoom(player.getCurrentRoom()).itemsInRoom.get(itemIndex).pickUp();
-                    getPlayer().backpack.add(getRoom(player.getCurrentRoom()).itemsInRoom.get(itemIndex));
-                } catch (IndexOutOfBoundsException e) {
-                    System.out.println("You must choose another index");
-                } catch (IllegalStateException i){
-                    System.out.println("Must choose a number");
-                }
-
-                break;
-            case "2": //Interact with object
+//            case "1": //Pick up an item
+//                System.out.println("Enter the index of the item: ");
+//                int itemIndex = sc.nextInt();
+//                try {
+//                    getRoom(player.getCurrentRoom()).itemsInRoom.get(itemIndex).pickUp();
+//                    getPlayer().backpack.add(getRoom(player.getCurrentRoom()).itemsInRoom.get(itemIndex));
+//                } catch (IndexOutOfBoundsException e) {
+//                    System.out.println("You must choose another index");
+//                } catch (IllegalStateException i){
+//                    System.out.println("Must choose a number");
+//                }
+//
+//                break;
+            case "1": //Interact with object
                 System.out.println("Enter the index of the object: ");
                 try {
                     int objectIndex = sc.nextInt();
@@ -64,7 +64,7 @@ public class Game {
                     System.out.println("Must choose a number");
                 }
                 break;
-            case "3": //Interact with npc
+            case "2": //Interact with npc
                 if(getRoom(player.getCurrentRoom()).getNpcInRoom() != null){
                     getRoom(player.getCurrentRoom()).getNpcInRoom().interact(getPlayer(), getRoom(getPlayer().getCurrentRoom()));
                 } else {
